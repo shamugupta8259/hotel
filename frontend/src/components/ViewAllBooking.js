b86418bb045b7043a250d2261b9ef9404514d02b
apple <105437783+shamugupta8259@users.noreply.github.com>
2024-03-09 19:03:35 +0530

    Update ViewAllBooking.js

--- frontend/src/components/ViewAllBooking.js
+++ frontend/src/components/ViewAllBooking.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useCallback } from "react";
 import TextField from "@mui/material/TextField";
-import { Button, Modal, Table, TableRow } from "flowbite-react";
+import { Button, Box, Modal, Table, TableRow } from "flowbite-react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
@@ -11,10 +11,21 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { MenuItem } from "@mui/material";
 import dayjs from "dayjs";
+import { Spinner } from "flowbite-react";
+
+import { Sidebar } from "flowbite-react";
+import {
+	HiArrowSmRight,
+	HiChartPie,
+	HiInbox,
+	HiShoppingBag,
+	HiTable,
+	HiUser,
+} from "react-icons/hi";
 
 const ViewAllBooking = () => {
 	const totalRooms = { A: [1, 2], B: [1, 2, 3], C: [1, 2, 3, 4, 5] };
-
+	const [spinner, setSpinner] = useState(false);
 	let postToBeDeleted = {};
 	const [bookingData, setBookingData] = useState({
 		email: "",
@@ -42,6 +53,7 @@ const ViewAllBooking = () => {
 			roomNumber: "",
 		});
 		try {
+			setSpinner(true);
 			const res = await fetch(
 				`https://hotel-production.up.railway.app/api/booking/viewallbook?startTime=${bookingData.startTime}&endTime=${bookingData.endTime}&roomNumber=${bookingData.roomNumber}&roomType=${bookingData.roomType}`,
 				{
@@ -55,6 +67,7 @@ const ViewAllBooking = () => {
 			} else {
 				console.log(data.error);
 			}
+			setSpinner(false);
 		} catch (error) {
 			console.log(error);
 		}
@@ -152,102 +165,102 @@ const ViewAllBooking = () => {
 		setShowMore(true);
 		fetchPosts();
 	};
-	// const handleEdit = () => {
-	//     if()
-	//     // to={`/update-post/${post._id}`}
-	// }
+
 	return (
 		<div className={`flex flex-row ${showModal ? "opacity-50" : ""}`}>
-			<div className="w-[100%] ">
-				<div className="table-auto overflow-x-scroll p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300">
+			{spinner && (
+				<div className="flex justify-center items-center">
+					<Spinner />
+				</div>
+			)}
+			<div className="w-[80%] ">
+				<div className="table-auto">
 					{booking && booking.length > 0 ? (
 						<>
-							<Table hoverable className="shadow-md">
-								<Table.Head className="text-lg  text-gray-700">
-									<Table.HeadCell>Created At</Table.HeadCell>
-									<Table.HeadCell>Room Type</Table.HeadCell>
-									<Table.HeadCell>Room Number</Table.HeadCell>
-									<Table.HeadCell>Check In Time</Table.HeadCell>
-									<Table.HeadCell>Check Out Time</Table.HeadCell>
-									<Table.HeadCell>Booking Price</Table.HeadCell>
-									<Table.HeadCell>Delete</Table.HeadCell>
-									<Table.HeadCell>Edit</Table.HeadCell>
-								</Table.Head>
-								{booking.map((post) => (
-									<Table.Body
-										className="divide-y border-2 rounded p-2"
-										key={post._id}
-									>
-										<Table.Row className="bg-white border-gray-200 text-gray-700">
-											<Table.Cell>
-												{new Date(post.updatedAt).toLocaleDateString()}
-											</Table.Cell>
-											<Table.Cell>{post.roomType}</Table.Cell>
-											<Table.Cell>{post.roomNumber}</Table.Cell>
-											<Table.Cell>{post.startTime}</Table.Cell>
-											<Table.Cell>{post.endTime}</Table.Cell>
-											<Table.Cell>
-												<span
-													className={`${
-														new Date(post.endTime) < new Date()
-															? "text-sky-800"
-															: "text-green-600"
-													} `}
-												>
-													<div className="font-semibold font-mono italic">
-														{post.price}
-													</div>
-													{new Date(post.endTime) < new Date() ? (
-														<>
+							<div className="container mx-auto">
+								<table className="table w-full shadow-md">
+									<thead className="text-lg text-gray-700">
+										<tr>
+											<th>Created At</th>
+											<th>Room Type</th>
+											<th>Room Number</th>
+											<th>Check In Date</th>
+											<th>Check Out Date</th>
+											<th>Booking Price</th>
+											<th>Delete</th>
+											<th>Edit</th>
+										</tr>
+									</thead>
+									<tbody>
+										{booking.map((post) => (
+											<tr
+												className="bg-white border-gray-200 text-gray-700"
+												key={post._id}
+											>
+												<td>{new Date(post.updatedAt).toLocaleDateString()}</td>
+												<td>{post.roomType}</td>
+												<td>{post.roomNumber}</td>
+												<td>
+													{new Date(post.startTime).toLocaleDateString("en-GB")}
+												</td>
+												<td>
+													{new Date(post.endTime).toLocaleDateString("en-GB")}
+												</td>
+												<td>
+													<span
+														className={`${
+															new Date(post.endTime) < new Date()
+																? "text-sky-800"
+																: "text-green-600"
+														}`}
+													>
+														<div className="font-semibold font-mono italic">
+															{post.price}
+														</div>
+														{new Date(post.endTime) < new Date() ? (
 															<div className="text-xs text-red-500">
 																Booking Completed
 															</div>
-														</>
-													) : (
-														<>
+														) : (
 															<div className="text-xs text-blue-500">
 																Booking
 															</div>
-														</>
-													)}
-												</span>
-											</Table.Cell>
-											<Table.Cell>
-												<span
-													onClick={() => {
-														postToBeDeleted = post;
-														handleDeleteMessage(post);
-													}}
-													className="font-medium text-red-500 hover:underline cursor-pointer"
-												>
-													Delete
-												</span>
-											</Table.Cell>
-											<Table.Cell>
-												<div
-													className="text-teal-500"
-													// to={`/update-post/${post._id}`}
-													// onClick={handleEdit}
-												>
-													{new Date(post.startTime) < new Date() ||
-													new Date(post.endTime) < new Date() ? (
-														<div className=" text-gray-600 flex-wrap w-14 h-12">
-															progress or completed
-														</div>
-													) : (
-														<Link
-															className="text-teal-500 hover:underline"
-															to={`/update-post/${post._id}`}
-														>
-															Edit
-														</Link>
-													)}
-												</div>
-											</Table.Cell>
-										</Table.Row>
-									</Table.Body>
-								))}
-							</Table>
+														)}
+													</span>
+												</td>
+												<td>
+													<span
+														onClick={() => {
+															postToBeDeleted = post;
+															handleDeleteMessage(post);
+														}}
+														className="font-medium text-red-500 hover:underline cursor-pointer"
+													>
+														Delete
+													</span>
+												</td>
+												<td>
+													<div className="text-teal-500">
+														{new Date(post.startTime) < new Date() ||
+														new Date(post.endTime) < new Date() ? (
+															<div className="text-gray-600 flex-wrap w-[5.5rem] h-12">
+																progress or completed
+															</div>
+														) : (
+															<Link
+																className="text-teal-500 hover:underline"
+																to={`/update-post/${post._id}`}
+															>
+																Edit
+															</Link>
+														)}
+													</div>
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
 						</>
 					) : (
 						<p>You have no posts yet!</p>
@@ -291,8 +304,8 @@ const ViewAllBooking = () => {
 					</Modal>
 				</div>
 			</div>
-			<div className="fixed right-0 z-100 bg-white">
-				<div className=" ">
+			<div className=" right-0 z-100 bg-white flex flex-col fixed">
+				<div className="">
 					<div>
 						<LocalizationProvider dateAdapter={AdapterDayjs}>
 							<DemoContainer components={["DateTimePicker"]}>
@@ -348,7 +361,7 @@ const ViewAllBooking = () => {
 							}}
 						>
 							<MenuItem key="Select" value="1">
-								Select Room
+								Select
 							</MenuItem>
 							{Object.keys(totalRooms).map((option) => (
 								<MenuItem key={option} value={option}>
@@ -372,8 +385,8 @@ const ViewAllBooking = () => {
 								});
 							}}
 						>
-							<MenuItem key="Select Room Number" value="0">
-								Select Room Number
+							<MenuItem key="Select Room Number " className="w-[20%]" value="0">
+								Select Room
 							</MenuItem>
 							{roomArray &&
 								roomArray.map((option) => (
@@ -382,22 +395,23 @@ const ViewAllBooking = () => {
 									</MenuItem>
 								))}
 						</TextField>
-					</div>
-					<div className="flex flex-row gap-4">
-						<Button
-							variant="contained"
-							onClick={handleSubmit}
-							className="text-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 text-white py-2 px-4 w-[8rem] mt-5 rounded h-12"
-						>
-							Filter
-						</Button>
-						<Button
-							variant="contained"
-							onClick={handleClear}
-							className="text-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 text-white py-2 px-4 w-[8rem] mt-5 rounded h-12"
-						>
-							Clear
-						</Button>
+
+						<div className="flex flex-row gap-4">
+							<Button
+								variant="contained"
+								onClick={handleSubmit}
+								className="text-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 text-white py-2 px-4 w-[8rem] mt-5 rounded h-12"
+							>
+								Filter
+							</Button>
+							<Button
+								variant="contained"
+								onClick={handleClear}
+								className="text-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 text-white py-2 px-4 w-[8rem] mt-5 rounded h-12"
+							>
+								Clear
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
